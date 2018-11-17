a77eee46e99649eb0b97e79cc613e461da7564bb
Feiyou <33148925+379490701@users.noreply.github.com>
2018-11-17 16:49:41 +0800

    提交时间 2018年11月17日16:49:38

--- HelloWorld/app/src/main/java/com/example/a37949/helloworld/HelloWorldActivity.java
+++ HelloWorld/app/src/main/java/com/example/a37949/helloworld/HelloWorldActivity.java
@@ -1,15 +1,25 @@
 package com.example.a37949.helloworld;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HelloWorldActivity extends AppCompatActivity {
 
-    private static String TAG = "LIFTCYCLE";
+    Button button1 = (Button) findViewById(R.id.button1);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HelloWorldActivity.this, NewActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
